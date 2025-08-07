e0ce1a975794aa7c8b82cd26e20220ef40fc0f6a
aleks <darklissik.darklissik@gmail.com>
2025-08-07 21:17:42 +0300

    Spring Data JPA Test [#505130]

---
+++ src/test/java/ru/job4j/socialmedia/repository/MessageRepositoryTest.java
@@ -0,0 +1,93 @@
+package ru.job4j.socialmedia.repository;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import ru.job4j.socialmedia.model.Message;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class MessageRepositoryTest {
+
+    @Autowired
+    private MessageRepository messageRepository;
+
+    @BeforeEach
+    public void setUp() {
+        messageRepository.deleteAll();
+    }
+
+    @BeforeAll
+    public void clearRepositories() {
+        messageRepository.deleteAll();
+    }
+
+    @Test
+    public void whenDontSaveMessageAndFoundItByIdThenReturnEmptyOptional() {
+        Optional<Message> foundMessage = messageRepository.findById(1L);
+        assertThat(foundMessage).isEmpty();
+    }
+
+    @Test
+    public void whenSaveMessageAndFoundItByIdThenReturnSavedMessage() {
+        Message savedMessage = new Message();
+        savedMessage.setContent("content");
+        messageRepository.save(savedMessage);
+        Optional<Message> foundMessage = messageRepository.findById(savedMessage.getId());
+        assertThat(foundMessage).isPresent();
+        assertThat(foundMessage.get().getContent()).isEqualTo("content");
+    }
+
+    @Test
+    public void whenDontSavedMessagesAndFoundAllThenReturnEmptyCollection() {
+        List<Message> messages = (List) messageRepository.findAll();
+        assertThat(messages).isEmpty();
+    }
+
+    @Test
+    public void whenSavedMessagesAndFoundAllThenReturnCollectionWithSavedMessages() {
+        Message savedMessage1 = new Message();
+        savedMessage1.setContent("content1");
+        Message savedMessage2 = new Message();
+        savedMessage2.setContent("content2");
+        messageRepository.save(savedMessage1);
+        messageRepository.save(savedMessage2);
+        List<Message> messages = (List) messageRepository.findAll();
+        assertThat(messages)
+                .isNotEmpty()
+                .hasSize(2)
+                .containsExactlyInAnyOrder(savedMessage1, savedMessage2);
+    }
+
+    @Test
+    public void whenSavedMessageAndDeleteItThenReturnEmptyOptional() {
+        Message savedMessage = new Message();
+        savedMessage.setContent("content");
+        messageRepository.save(savedMessage);
+        messageRepository.deleteById(savedMessage.getId());
+        Optional<Message> foundMessage = messageRepository.findById(savedMessage.getId());
+        assertThat(foundMessage).isEmpty();
+    }
+
+    @Test
+    public void whenSavedMessagesAndDeleteAllThenReturnEmptyCollection() {
+        Message savedMessage1 = new Message();
+        savedMessage1.setContent("content1");
+        Message savedMessage2 = new Message();
+        savedMessage2.setContent("content2");
+        messageRepository.save(savedMessage1);
+        messageRepository.save(savedMessage2);
+        messageRepository.deleteAll();
+        List<Message> messages = (List) messageRepository.findAll();
+        assertThat(messages)
+                .isEmpty();
+    }
+}
---
+++ src/test/java/ru/job4j/socialmedia/repository/UserRepositoryTest.java
@@ -0,0 +1,106 @@
+package ru.job4j.socialmedia.repository;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import ru.job4j.socialmedia.model.User;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @BeforeEach
+    public void setUp() {
+        userRepository.deleteAll();
+    }
+
+    @BeforeAll
+    public void clearRepositories() {
+        userRepository.deleteAll();
+    }
+
+    @Test
+    public void whenDontSaveUserAndFoundItByIdThenReturnEmptyOptional() {
+        Optional<User> foundUser = userRepository.findById(1L);
+        assertThat(foundUser).isEmpty();
+    }
+
+    @Test
+    public void whenSaveUserAndFoundItByIdThenReturnSavedUser() {
+        User savedUser = new User();
+        savedUser.setLogin("login");
+        savedUser.setName("vova");
+        savedUser.setPassword("psw");
+        userRepository.save(savedUser);
+        Optional<User> foundUser = userRepository.findById(savedUser.getId());
+        assertThat(foundUser).isPresent();
+        assertThat(foundUser.get().getLogin()).isEqualTo("login");
+    }
+
+    @Test
+    public void whenDontSavedUserAndFoundAllThenReturnEmptyCollection() {
+        List<User> users = (List) userRepository.findAll();
+        assertThat(users).isEmpty();
+    }
+
+    @Test
+    public void whenSavedUserAndFoundAllThenReturnCollectionWithSavedUsers() {
+        User savedUser1 = new User();
+        savedUser1.setLogin("login1");
+        savedUser1.setName("vova");
+        savedUser1.setPassword("psw");
+        User savedUser2 = new User();
+        savedUser2.setLogin("login2");
+        savedUser2.setName("vova");
+        savedUser2.setPassword("psw");
+        userRepository.save(savedUser1);
+        userRepository.save(savedUser2);
+        List<User> users = (List) userRepository.findAll();
+        assertThat(users)
+                .isNotEmpty()
+                .hasSize(2)
+                .containsExactlyInAnyOrder(savedUser1, savedUser2);
+    }
+
+    @Test
+    public void whenSavedUserAndDeleteItThenReturnEmptyOptional() {
+        User savedUser1 = new User();
+        savedUser1.setLogin("login1");
+        savedUser1.setName("vova");
+        savedUser1.setPassword("psw");
+        userRepository.save(savedUser1);
+        userRepository.deleteById(savedUser1.getId());
+        Optional<User> foundUser = userRepository.findById(savedUser1.getId());
+        assertThat(foundUser).isEmpty();
+    }
+
+    @Test
+    public void whenSavedUsersAndDeleteAllThenReturnEmptyCollection() {
+        User savedUser1 = new User();
+        savedUser1.setLogin("login1");
+        savedUser1.setName("vova");
+        savedUser1.setPassword("psw");
+        User savedUser2 = new User();
+        savedUser2.setLogin("login2");
+        savedUser2.setName("vova");
+        savedUser2.setPassword("psw");
+        userRepository.save(savedUser1);
+        userRepository.save(savedUser2);
+        userRepository.deleteAll();
+        List<User> users = (List) userRepository.findAll();
+        assertThat(users)
+                .isEmpty();
+    }
+}
+
