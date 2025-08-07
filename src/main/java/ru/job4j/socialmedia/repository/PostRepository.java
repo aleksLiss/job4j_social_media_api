a00e7a74c11df880a6439755e7f8eede20e844bb
aleks <darklissik.darklissik@gmail.com>
2025-08-07 23:37:51 +0300

    HQL, JPQL, Native SQL [#505064]

--- src/main/java/ru/job4j/socialmedia/repository/PostRepository.java
+++ src/main/java/ru/job4j/socialmedia/repository/PostRepository.java
@@ -2,17 +2,44 @@ package ru.job4j.socialmedia.repository;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.job4j.socialmedia.model.Post;
 
 import java.util.Collection;
 import java.util.Date;
 
-public interface PostRepository extends CrudRepository<Post, Long> {
+public interface PostRepository extends JpaRepository<Post, Long> {
 
     Collection<Post> findByUserId(long id);
 
     Collection<Post> findByCreatedGreaterThanEqualAndCreatedLessThanEqual(Date startCreated, Date finishCreated);
 
     Page<Post> findByOrderByCreatedDesc(Pageable pageable);
+
+    @Modifying(clearAutomatically = true)
+    @Query(value = """
+            UPDATE Posts 
+            SET title = :postTitle, description = :postDescription
+            WHERE id = :postId
+            """, nativeQuery = true)
+    int updateTitleAndDescription(@Param("postTitle") String title,
+                                  @Param("postDescription") String description,
+                                  @Param("postId") long postId);
+
+    @Modifying(clearAutomatically = true)
+    @Query(value = """
+            UPDATE Posts
+            SET path_to_file = NULL
+            WHERE id = :postId
+            """, nativeQuery = true)
+    int deleteImageFromPost(@Param("postId") long postId);
+
+    @Modifying(clearAutomatically = true)
+    @Query(value = """
+            DELETE FROM Posts WHERE id = :postId
+            """, nativeQuery = true)
+    int deletePost(@Param("postId") long postId);
 }
--- src/main/java/ru/job4j/socialmedia/repository/UserRepository.java
+++ src/main/java/ru/job4j/socialmedia/repository/UserRepository.java
@@ -1,7 +1,38 @@
 package ru.job4j.socialmedia.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import ru.job4j.socialmedia.model.Post;
 import ru.job4j.socialmedia.model.User;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+import java.util.Collection;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query(value = """
+            SELECT * FROM Users WHERE login = :userLogin AND password = :userPassword
+            """, nativeQuery = true)
+    Optional<User> findByLoginAndPassword(@Param("userLogin") String login,
+                                          @Param("userPassword") String password);
+
+    @Query(value = """
+            SELECT * FROM Followers WHERE following_user_id = :userId
+            """, nativeQuery = true)
+    Collection<User> getAllSubscribers(@Param("userId") long userId);
+
+    @Query(value = """
+            SELECT * FROM Friends WHERE from_user_id = :userId
+            """, nativeQuery = true)
+    Collection<User> getAllFriends(@Param("userId") long userId);
+
+    @Query(value = """
+            SELECT * FROM posts WHERE user_id
+            IN (SELECT follower_user_id FROM Followers WHERE following_user_id = :userId)
+            """, nativeQuery = true)
+    Page<Post> findByOrderByCreatedDesc(@Param("userId") long userId, Pageable pageable);
 }
